6ea6c434b7227014cf1c8fa51ab4f6f3c46f339f
Marc Morera <yuhu@mmoreram.com>
2018-08-22 04:10:53 +0200

    Updated one-bunlde-app deps
    - Improved server output
    - Favicon is no longer being registered by the server

--- docker/server-entrypoint.sh
+++ docker/server-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 php /var/www/apisearch/bin/console apisearch-server:server-configuration --env=prod
-php /var/www/apisearch/bin/server 0.0.0.0:8200
+php /var/www/apisearch/bin/server 0.0.0.0:8200 --api
