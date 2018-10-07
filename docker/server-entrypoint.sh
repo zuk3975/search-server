6d2512968c6f235532b973dd0dc7e85eda4c0505
Marc Morera <yuhu@mmoreram.com>
2018-10-08 01:14:04 +0200

    Fixed cache deletion for docker
    - Deleted skipped and unused tests

--- docker/server-entrypoint.sh
+++ docker/server-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -Rf /var/www/apisearch/var/cache
+php /var/www/apisearch/bin/console cache:warmup --env=prod
 php /var/www/apisearch/bin/console apisearch-server:server-configuration --env=prod
 php /var/www/apisearch/bin/server 0.0.0.0:8200 --api
